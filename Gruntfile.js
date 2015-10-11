e469af66809c6b0785c62638e7cfa8355b4b434f
Benoit AVERTY <benoit.averty@gmail.com>
2015-10-11 11:23:20 +0200

    init dependencies for unit testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,27 @@ module.exports = function(grunt) {
     wiredep: {
       app: {
         src: ['src/main/webapp/index.html'],
+      },
+      test: {
+        src: ['src/test/javascript/karma.conf.js'],
+        devDependencies: true
+      }
+    },
+    karma: {
+      unit: {
+        configFile: 'src/test/javascript/karma.conf.js',
+        singleRun: 'true'
       }
     }
   });
 
   // Load the plugin that provides the "wiredep" task.
   grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('karma');
 
   // Default task(s).
-  grunt.registerTask('default', ['wiredep']);
+  grunt.registerTask('build', ['wiredep:app']);
+
+  grunt.registerTask('test', ['wiredep:test','karma']);
 
 };
