98131253691999837bcd4570af115dd14a609a96
Benoit AVERTY <benoit.averty@gmail.com>
2015-10-26 18:46:30 +0100

    Use relative location in jenkins script

--- src/main/sh/jenkins-deploy.sh
+++ src/main/sh/jenkins-deploy.sh
@@ -2,15 +2,15 @@
 JAVA=$(which java)
 ARGS="-jar $WORKSPACE/target/webcompiler-0.0.1-SNAPSHOT.war --spring.profiles.active=prod"
 COMMAND="$JAVA $ARGS"
-PID=$(ps -ef | grep $COMMAND | grep -v grep | tr -s " " | cut -f2 -d" ")
+PID=$(ps -ef | grep '$COMMAND' | grep -v grep | tr -s " " | cut -f2 -d" ")
 
 # Kill running process
 [ ! -z "$PID" ] && kill -INT $PID
 
 while [[ ! -z "$PID" ]]; do
     sleep 5
-    PID=$(ps -ef | grep $COMMAND | grep -v grep | tr -s " " | cut -f2 -d" ")
+    PID=$(ps -ef | grep '$COMMAND' | grep -v grep | tr -s " " | cut -f2 -d" ")
 done
 
 #start it
-eval nohup $COMMAND &
+eval nohup '$COMMAND' &
